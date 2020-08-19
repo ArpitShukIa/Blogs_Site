dd5e9d696251b8189a5d7f96fed957a7346fad4d
Arpit Shukla <arpitshukla62@gmail.com>
2020-08-19 21:48:56 +0530

    Create pagination

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
